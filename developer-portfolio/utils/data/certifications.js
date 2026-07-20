7c2ed64d56fce3723e8a436e3ff2cf085db58bc4
Nikhil Mundhra <nikhilmundhra28@gmail.com>
2026-07-21 00:25:27 +0530

    feat: include all certifications and awards

--- developer-portfolio/utils/data/certifications.js
+++ developer-portfolio/utils/data/certifications.js
@@ -5,18 +5,54 @@ export const certifications = [
   },
   {
     id: 2,
-    title: "IGCSE",
-  },
-  {
-    id: 3,
     title: "LinkedIn Marketing Strategy",
   },
   {
-    id: 4,
+    id: 3,
     title: "NYU Startup Bootcamp",
   },
   {
-    id: 5,
+    id: 4,
     title: "IBDP",
+  },
+  {
+    id: 5,
+    title: "IGCSE",
+  },
+  {
+    id: 6,
+    title: "Bronze Medalist & 2-time SuperSprint Triathlon finisher @ TriYas",
+  },
+  {
+    id: 7,
+    title: "UAE Top 3 Adaptive City Hackathon Award Winner",
+  },
+  {
+    id: 8,
+    title: "Distinction Certificate on CEMC Waterloo CCC",
+  },
+  {
+    id: 9,
+    title: "Batch of Recognition as Math Club President @ JPIS",
+  },
+  {
+    id: 10,
+    title: "Batch of Recognition as School Captain @ JPIS",
+  },
+  {
+    id: 11,
+    title: "Certificate of Appreciation for DHEERA (No to VAW) Program",
+  },
+  {
+    id: 12,
+    title: "Star Performer @ CRY NGO",
+  },
+  {
+    id: 13,
+    title: "Hall of Fame for acquiring A*s in all IGCSE Subjects",
+  },
+  {
+    id: 14,
+    title: "National Finalist @ RoboCup Junior",
   }
 ];
