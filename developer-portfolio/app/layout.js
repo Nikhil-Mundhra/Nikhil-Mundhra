19dff24694409d89d10b96eee4ae54db7360353e
Nikhil Mundhra <nikhilmundhra28@gmail.com>
2026-07-21 00:24:04 +0530

    chore: change website favicon to profile picture

--- developer-portfolio/app/layout.js
+++ developer-portfolio/app/layout.js
@@ -13,6 +13,9 @@ export const metadata = {
   title: "Portfolio of Nikhil Mundhra - Software Developer",
   description:
     "This is the portfolio of Nikhil Mundhra. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+  icons: {
+    icon: "/profile.png",
+  },
 };
 
 export default function RootLayout({ children }) {
